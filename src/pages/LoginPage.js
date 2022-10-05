55bf17e0ef2e6563b5acda3c65d612f91d8f9c78
starbuckschair <water1793@gmail.com>
2022-10-05 17:43:14 +0900

    pages/LoginPage 로그인 기능 구현(서버통신확인완료

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
 
 let PaddingBox = styled.div`
     height: 60px;
@@ -13,7 +15,6 @@ const Background = styled.div`
   justify-content: center;
   border: 1px solid red;
 `;
-
 const ContentsBox = styled.div`
   width: 500px;
   height: 500px;
@@ -25,7 +26,6 @@ const ContentsBox = styled.div`
     margin-top: 0;
   }
 `;
-
 const InputBox = styled.input`
   width: 300px;
   height: 25px;
@@ -62,13 +62,48 @@ const LoginButton = styled.button`
   margin-bottom: 10px;
 `;
 
-function LoginPage() {
-  const [Email, setEmail] = useState('');
-  const [Password, setPassword] = useState('');
-  const [isChecked, setIsChecked] = useState(false);
 
-  const handleChecked = (event) => {
-    setIsChecked(event.target.checked);
+function LoginPage({setIsLogin, setUserInfo}) {
+  // const [Email, setEmail] = useState('');
+  // const [Password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const[loginInfo, setLoginInfo] = useState({
+    userId: '',
+    password: '',
+  });
+  const [checkedKeepLogin, setCheckedKeepLogin] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const SignIn =() => {
+    navigate('/')
+  }
+  const handleInputValue = (key) => (e) => {
+    setLoginInfo({ ...loginInfo, [key]: e.target.value });
+  };
+  const loginRequestHandler = () => {
+    // Login 컴포넌트가 가지고 있는 state를 이용해 로그인을 구현합니다.
+    // 로그인에 필요한 유저정보가 충분히 제공되지 않았다면 에러메시지가 나타남  
+
+    if(!loginInfo.userId|| !loginInfo.password){
+      setErrorMessage('아이디와 비밀번호를 입력하세요')
+      return;
+    } else {
+      setErrorMessage("")
+    }
+    return axios
+      .post("http://192.168.4.161:8080/v1/members/login",{loginInfo, checkedKeepLogin})
+      .then((res) => {
+        // 로그인에 성공했다면 응답으로 받은 데이터가 Mypage에 렌더링되도록 State를 변경하세요.
+        console.log(res.data)
+        // setUserInfo(res.data)
+        // setIsLogin(true)
+        SignIn('/')
+      })
+      .catch((err) => {
+        console.log(err.response.data)
+        setErrorMessage("로그인에 실패했습니다.")
+        // 로그인에 실패했다면 그에 대한 에러 핸들링을 구현하세요. 
+      });
+      
   };
 
   return (
@@ -80,16 +115,14 @@ function LoginPage() {
           <h2>로그인</h2>
           <h4>호박공구마 서비스를 이용하기 위해 로그인해주세요.</h4>
           <p>아이디</p>
-          <InputBox placeholder="이메일주소 입력"></InputBox>
+          <InputBox type="text" placeholder="이메일주소 입력" onChange={handleInputValue('userId')}></InputBox>
           <p>비밀번호</p>
-          <InputBox placeholder="비밀번호 입력"></InputBox>
+          <InputBox type="password" placeholder="비밀번호 입력" onChange={handleInputValue('password')}></InputBox>
           <CheckZone>
             <div>
             <input
               type="checkbox"
-              checked={isChecked}
-              onChange={handleChecked}
-            ></input>
+              onChange={()=>setCheckedKeepLogin(!checkedKeepLogin)} />
             <p>아이디저장</p>
             </div>
             <div>
@@ -98,7 +131,16 @@ function LoginPage() {
             </div>
           </CheckZone>
           <ButtonZone>
-            <LoginButton>로그인</LoginButton>
+            <LoginButton type="submit" onClick={loginRequestHandler}>
+              로그인
+            </LoginButton>
+              {errorMessage ? (
+            <div id='alert-message' data-testid='alert-message'>
+              {errorMessage}
+            </div>
+          ) : (
+            ''
+          )}
             <LoginButton>카카오톡 로그인</LoginButton>
           </ButtonZone>
         </ContentsBox>
